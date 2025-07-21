cbde651ba21a6c70e8e27e1f5a521f8834c67947
rafasumi <rafael.sumitani@gmail.com>
2025-07-21 14:11:08 -0300

    Add pass pipeline example

--- matmul-opt.cpp
+++ matmul-opt.cpp
@@ -1,4 +1,7 @@
 #include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/Linalg/IR/Linalg.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
+#include "mlir/InitAllDialects.h"
 #include "mlir/InitAllPasses.h"
 #include "mlir/Pass/PassManager.h"
 #include "mlir/Pass/PassOptions.h"
@@ -6,8 +9,10 @@
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
 
 int main(int argc, char **argv) {
+  mlir::registerAllPasses();
+
   mlir::DialectRegistry registry;
-  registry.insert<mlir::BuiltinDialect, mlir::arith::ArithDialect>();
+  mlir::registerAllDialects(registry);
 
   return failed(mlir::MlirOptMain(argc, argv, "SBLP 2025 MLIR Tutorial test\n",
                                   registry));
